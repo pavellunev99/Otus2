d3f820288c596a63886a57c59898e7eb79b4af39
Pavel Lunev <p.lunev@incrdbl.me>
2022-01-11 15:16:23 +0500

    Initial Commit

---
+++ Otus2/Otus2App.swift
@@ -0,0 +1,17 @@
+//
+//  Otus2App.swift
+//  Otus2
+//
+//  Created by Павел Лунев on 11.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Otus2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
